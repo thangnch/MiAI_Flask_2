bac2234243fe2ee35bf2108f11ac1bf46056d291
thangnch <thangnch@gmail.com>
2019-10-22 16:37:56 +0700

    Add files via upload

---
+++ client/MiAI/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace MiAI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(668, 9);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Send to Server";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(197, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "NGUYEN CHIEN THANG";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.InitialImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 38);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(776, 400);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ client/MiAI/Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MiAI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Ham goi HTTP Get len server
+        public string sendGet(string uri)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+            request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+
+        // Ham chuyen Image thanh Base 64
+        public static string ConvertImageToBase64String(Image image)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+               
+                image.Save(ms, System.Drawing.Imaging.ImageFormat.Bmp);
+                return Convert.ToBase64String(ms.ToArray());
+            }
+        }
+
+        // Ham convert B64 de gui len server
+        private String EscapeData(String B64)
+        {
+            int B64_length = B64.Length;
+            if (B64_length <= 32000)
+            {
+                return Uri.EscapeDataString(B64);
+            }
+
+
+            int idx = 0;
+            StringBuilder builder = new StringBuilder();
+            String substr = B64.Substring(idx, 32000);
+            while (idx < B64_length)
+            {
+                builder.Append(Uri.EscapeDataString(substr));
+                idx += 32000;
+
+                if (idx < B64_length)
+                {
+
+                    substr = B64.Substring(idx, Math.Min(32000, B64_length - idx));
+                }
+
+            }
+            return builder.ToString();
+
+        }
+
+        // Ham goi HTTP POST len server de detect
+        private String sendPOST(String url, String B64)
+        {
+            try
+            {
+
+                var request = (HttpWebRequest)WebRequest.Create(url);
+                request.Timeout = 5000;
+                var postData = "image=" + EscapeData(B64);
+
+                var data = Encoding.ASCII.GetBytes(postData);
+
+                request.Method = "POST";
+                request.ContentType = "application/x-www-form-urlencoded";
+                request.ContentLength = data.Length;
+
+                using (var stream = request.GetRequestStream())
+                {
+                    stream.Write(data, 0, data.Length);
+                }
+
+                var response = (HttpWebResponse)request.GetResponse();
+
+                var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+
+                return responseString;
+            }
+            catch (Exception ex)
+            {
+                return "Exception" + ex.ToString();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Doc du lieu cac ten class tu file yolov3.txt 
+            string[] lines = File.ReadAllLines("yolov3.txt");
+
+            // Convert image to B64
+            String B64 = ConvertImageToBase64String(pictureBox1.Image);
+           
+            // Goi len server va tra ve ket qua
+            String server_ip = "192.168.8.123";
+            String server_path = "http://" + server_ip + ":8000/detect";
+            String retStr = sendPOST(server_path, B64);
+
+           
+            // Ve cac khung chu nhat va ten class len anh 
+            Graphics newGraphics = Graphics.FromImage(pictureBox1.Image);
+
+            String[] items = retStr.Split('|');
+            for (int idx=0;idx<items.Length-1;idx++)
+            {
+                String[] val = items[idx].Split(',');
+                // Draw it
+                Pen blackPen = new Pen(Color.Black, 2);
+
+                // Create rectangle.
+                Rectangle rect = new Rectangle(int.Parse(val[1]), int.Parse(val[2]), int.Parse(val[3]), int.Parse(val[4]));
+
+                // Draw rectangle to screen.
+                newGraphics.DrawRectangle(blackPen, rect);
+                newGraphics.DrawString(lines[int.Parse(val[0])], new Font("Tahoma", 8), Brushes.Black, rect);
+
+            }
+
+            pictureBox1.Refresh();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Resize anh cua picture box 1 de dam bao dung scale
+            Image image = (Image)(new Bitmap(pictureBox1.Image, new Size(pictureBox1.Width, pictureBox1.Height)));
+            pictureBox1.Image = image;
+        }
+    }
+}
